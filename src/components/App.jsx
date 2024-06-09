3e0d2ece441a2c1b0eb0858327fdd4f11d9cd4cd
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-06-09 12:09:29 +0800

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Profile } from './Profile/Profile';
 import user from '../components/data/user.json';
-import { Statistics } from './Statistics/Statistics';
-import data from '../components/data/data.json';
+
 export const App = () => {
   return (
     <>
@@ -12,8 +11,6 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
-      <Statistics title="Upload Stats" Stats={data} />
     </>
   );
 };
